d06aeec3efe32a5bd7d07ffe427c4a66738e879f
Nikita Reutov <liljollier@gmail.com>
2019-12-31 12:14:18 +0200

    feat(home:drum-pad): add drum-pad component

---
+++ src/app/home/drum-pad/drum-pad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drum-pad',
+  templateUrl: './drum-pad.component.html',
+  styleUrls: ['./drum-pad.component.scss']
+})
+export class DrumPadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

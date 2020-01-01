d3bcf54a2feaa30086fc844b270ca971d97f7d12
Nikita Reutov <liljollier@gmail.com>
2020-01-01 20:13:54 +0200

    feat(home:drum-pad): set sound src as input property, remove console.log

--- src/app/home/drum-pad/drum-pad.component.ts
+++ src/app/home/drum-pad/drum-pad.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-drum-pad',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DrumPadComponent implements OnInit {
 
-  soundsSrs = '../../../assets/audio/kick.wav';
+  @Input()
+  soundsSrs: string = '../../../assets/audio/kick.wav';
 
   audio: HTMLAudioElement;
 
@@ -17,7 +18,6 @@ export class DrumPadComponent implements OnInit {
   ngOnInit() {
     this.audio = new Audio();
     this.audio.src = this.soundsSrs;
-    console.log(this.audio);
   }
 
   click() {

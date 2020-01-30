c03279fd070a8065713fa8fc9653882b2a8a3b1d
Nikita Reutov <liljollier@gmail.com>
2020-01-30 20:08:07 +0200

    feat(shared): add material icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { TranslateModule } from '@ngx-translate/core';
 import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [PageNotFoundComponent, WebviewDirective],
-  imports: [CommonModule, TranslateModule, FormsModule],
-  exports: [TranslateModule, WebviewDirective, FormsModule]
+  imports: [CommonModule, TranslateModule, FormsModule, MatIconModule],
+  exports: [TranslateModule, WebviewDirective, FormsModule, MatIconModule]
 })
 export class SharedModule {}

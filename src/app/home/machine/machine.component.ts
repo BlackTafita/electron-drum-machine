d42ea5a82296c80fa9db51203d1a23a9769e7e2c
Nikita Reutov <liljollier@gmail.com>
2020-03-30 01:42:44 +0300

    feat(home:machine): add metronome

--- src/app/home/machine/machine.component.ts
+++ src/app/home/machine/machine.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-machine',
@@ -8,10 +10,33 @@ import { Component, OnInit } from '@angular/core';
 export class MachineComponent implements OnInit {
 
   active: boolean = false;
+  metronomeBPM: number = 120;
+  metronomeAudioSrc: string = 'assets/audio/hat.wav';
+  metronomeAudio: HTMLAudioElement;
+
+  metronome$: Observable<number>;
+
+  get metronomeMs() {
+    return Number((1000 / (this.metronomeBPM / 60)).toFixed());
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.metronomeAudio = new Audio();
+    this.metronomeAudio.src = this.metronomeAudioSrc;
+    this.metronomeAudio.load();
+
+    interval(this.metronomeMs).pipe(
+      filter(() => this.active),
+    ).subscribe((i) => {
+      if (i % 2) {
+        this.metronomeAudio.load();
+        this.metronomeAudio.play();
+      } else {
+        this.metronomeAudio.pause();
+      }
+    });
   }
 
   toggle() {
